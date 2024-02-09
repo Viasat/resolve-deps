7d178e500952e879e9be60540658d4924b76561e
Joel Martin <github@martintribe.org>
2024-02-09 14:14:07 -0600

    tests/resolve-deps.sh: support N expected strings
    Fix the "e c z" test by adding the other possible ordering as a third
    expected string.

--- tests/resolve-deps.sh
+++ tests/resolve-deps.sh
@@ -4,20 +4,23 @@ die() { echo "${*}"; exit 1; }
 RESOLVE_DEPS="${1}"; shift || die "Usage: ${0} resolve-deps-path"
 
 test_rd() {
-  local args="${1}" expected1="${2}" expected2="${3:-${expected1}}" res=
+  local res= args="${1}" expected=; shift
   res=$(${RESOLVE_DEPS} tests/resolve-deps/modes "${args}" 2>/dev/null)
-  if [ "${res}" = "${expected1}" -o "${res}" = "${expected2}"  ]; then
-      echo "PASS: ${args} => ${res}"
-  else
-      echo "FAIL: ${args} => ${res} (expected: ${expected1})"
-  fi
+  for expected in "${@}"; do
+    if [ "${res}" = "${expected}" ]; then
+        echo "PASS: ${args} => ${res}"
+        return
+    fi
+  done
+  echo "FAIL: ${args} => ${res} (expected: ${expected})"
 }
 
-test_rd "a"     "a"          # no deps file
-test_rd "z"     "z"          # no mode directory at all
-test_rd "b"     "a b"        # simple one level dep
-test_rd "e"     "a b d e"    # pick first alternate
-test_rd "e c"   "a b c e"    # force other alternate
-test_rd "e c a" "a b c e"    # eliminate redundant start and deps
-test_rd "e c z" "a b c z e" "a b c e z"  # include no deps files item
-test_rd "cyc1"  ""           # should error and return empty
+test_rd "a"     "a"             # no deps file
+test_rd "z"     "z"             # no mode directory at all
+test_rd "b"     "a b"           # simple one level dep
+test_rd "e"     "a b d e"       # pick first alternate
+test_rd "e c"   "a b c e"       # force other alternate
+test_rd "e c a" "a b c e"       # eliminate redundant start and deps
+test_rd "e c z" "a b c z e" "a b c e z" "a z b c e" "a b z c e"  # include no deps files item
+
+test_rd "cyc1"  ""              # should error and return empty
