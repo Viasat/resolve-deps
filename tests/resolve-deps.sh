77f5e4bd1079eb954e77dc1483cb2b4144b0c12a
Joel Martin <github@martintribe.org>
2024-02-09 14:18:08 -0600

    [tests/resolve-deps.sh] add overall exit code
    If any individual tests fail then return 1/fail as the overall exit
    code.
    
    Also, add an additional test "c" that should resolve to "a b c".

--- tests/resolve-deps.sh
+++ tests/resolve-deps.sh
@@ -2,6 +2,7 @@
 
 die() { echo "${*}"; exit 1; }
 RESOLVE_DEPS="${1}"; shift || die "Usage: ${0} resolve-deps-path"
+fails=0
 
 test_rd() {
   local res= args="${1}" expected=; shift
@@ -13,14 +14,18 @@ test_rd() {
     fi
   done
   echo "FAIL: ${args} => ${res} (expected: ${expected})"
+  fails=$(( fails + 1 ))
 }
 
 test_rd "a"     "a"             # no deps file
 test_rd "z"     "z"             # no mode directory at all
 test_rd "b"     "a b"           # simple one level dep
+test_rd "c"     "a b c"         # two level deps
 test_rd "e"     "a b d e"       # pick first alternate
 test_rd "e c"   "a b c e"       # force other alternate
 test_rd "e c a" "a b c e"       # eliminate redundant start and deps
 test_rd "e c z" "a b c z e" "a b c e z" "a z b c e" "a b z c e"  # include no deps files item
 
 test_rd "cyc1"  ""              # should error and return empty
+
+[ "${fails}" -eq 0 ]
