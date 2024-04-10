c7adf9c080b6c426ece520877162e136f0fdba4b
Joel Martin <github@martintribe.org>
2024-04-10 17:06:56 -0500

    Add --path (one or more dep paths) and --format (output format).
    - The --path argument has a RESOLVE_DEPS_PATH environment variable
      fallback and default value of "./". The path value is a colon
      separated value. Each path component will be searched for dep
      directories. If a dep node appears in more than one path directory
      then an error will be thrown/raised.
    - The --format argument takes three choices:
        - "nodes": print node names in order on one line (default)
        - "paths": print "node=path" format as separate lines
        - "json": print JSON list of node data maps
    - Restructure the tests directory to have four sub-directories for
      testing dep paths: basic, order, cycle, dupes. Move the deps directories
      from tests/resolve-deps/modes into those directories. Split the tests
      in tests/resolve-deps.sh into those new directories and set
      RESOLVE_DEPS_PATH before each test group.
    - Add tests to check for duplicate dep dirs.
    - Use regex tests in tests/resolve-deps.sh. Capture error output too and
      use the regex matches to match those for expected failures.

--- tests/resolve-deps.sh
+++ tests/resolve-deps.sh
@@ -6,9 +6,9 @@ fails=0
 
 test_rd() {
   local res= args="${1}" expected=; shift
-  res=$(${RESOLVE_DEPS} tests/resolve-deps/modes "${args}" 2>/dev/null)
+  res=$(${RESOLVE_DEPS} "${args}" 2>&1)
   for expected in "${@}"; do
-    if [ "${res}" = "${expected}" ]; then
+    if [[ "${res}" =~ ${expected} ]]; then
         echo "PASS: ${args} => ${res}"
         return
     fi
@@ -17,23 +17,30 @@ test_rd() {
   fails=$(( fails + 1 ))
 }
 
-test_rd "a"     "a"             # no deps file
-test_rd "z"     "z"             # no mode directory at all
-test_rd "b"     "a b"           # simple one level dep
-test_rd "c"     "a b c"         # two level deps
-test_rd "e"     "a b d e"       # pick first alternate
-test_rd "e c"   "a b c e"       # force other alternate
-test_rd "e c a" "a b c e"       # eliminate redundant start and deps
-test_rd "e c z" "a b c z e" "a b c e z" "a z b c e" "a b z c e"  # include no deps files item
+export RESOLVE_DEPS_PATH=tests/basic
+test_rd "a"     "^a$"               # no deps file
+test_rd "z"     "^z$"               # no mode directory at all
+test_rd "b"     "^a b$"             # simple one level dep
+test_rd "c"     "^a b c$"           # two level deps
+test_rd "e"     "^a b d e$"         # pick first alternate
+test_rd "e c"   "^a b c e$"         # force other alternate
+test_rd "e c a" "^a b c e$"         # eliminate redundant start and deps
+test_rd "e c z" "^a b c z e$" "^a b c e z$" "^a z b c e$" "^a b z c e$"  # include no deps files item
 
-test_rd "cyc1"  ""              # should error and return empty
+export RESOLVE_DEPS_PATH=tests/order
+test_rd "o1"    "^o1$"              # order/weak dep on o3 should be ignored
+test_rd "o1 o2" "^o1 o2$" "^o2 o1$" # no ordering
+test_rd "o2 o1" "^o1 o2$" "^o2 o1$" # no ordering
+test_rd "o4"    "^o4$"              # order/weak dep on o5 should be ignored
+test_rd "o3"    "^o5 o4 o3$"        # mix of ordering types
+test_rd "o2 o3" "^o5 o4 o3 o2$"     # mix of ordering types
+test_rd "o1 o3" "^o5 o4 o3 o1$"     # mix of ordering types
 
-test_rd "o1"    "o1"            # order/weak dep on o3 should be ignored
-test_rd "o1 o2" "o1 o2" "o2 o1" # no ordering
-test_rd "o2 o1" "o1 o2" "o2 o1" # no ordering
-test_rd "o4"    "o4"            # order/weak dep on o5 should be ignored
-test_rd "o3"    "o5 o4 o3"      # mix of ordering types
-test_rd "o2 o3" "o5 o4 o3 o2"   # mix of ordering types
-test_rd "o1 o3" "o5 o4 o3 o1"   # mix of ordering types
+export RESOLVE_DEPS_PATH=tests/cycle
+test_rd "cyc1"  "^Error:.*cycle"    # should error and return empty
+
+export RESOLVE_DEPS_PATH=tests/basic:tests/dupes
+test_rd "a"  "^Error:.*multiple"    # should error and return empty
+test_rd "c"  "^Error:.*multiple"    # should error and return empty
 
 [ "${fails}" -eq 0 ]
